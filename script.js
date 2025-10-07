638f66b1e49b48e6c54286471f0eae2d6ee4fca1
Deny6243 <saidodilkhon12@gmail.com>
2025-10-07 19:32:01 +0500

    script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 const students = [
   "Al Orifxo'ja Said Odilxon Said Olimxon o'g'li",
   "Ashurov Humoyun Zamir O'g'li",
-  "	Botiraliyeva Aziza Erali qizi",
+  "Botiraliyeva Aziza Erali qizi",
   "Davranov Muhammadiso Jamol o’g’li",
   "Fayzullayev Elshodbek Elbek o’g’li",
   "Husanova Mushtariy Ergash qizi",
   "Ilxamboyev Xurrambek Gulomjon O'g'li",
   "Ismoilov Muslimbek Isomiddinovich",
   "Jurayeva Dilnura Rustamovna",
-  "Matyoqubova Madinabonu G`ayrat qizi",
+  "Matyoqubova Madinabonu G‘ayrat qizi",
   "Muxtarov Sunnatxon Batir o’g’li",
   "Nasrullayeva Hadicha Talibjonovna",
   "Ochilov Mo'minjon Ma'ruf O'g'li",
@@ -21,14 +21,14 @@ const students = [
   "Tuxtasinova Madinabonu Murodjon qizi",
   "Urazbekova Diana Toxir qizi",
   "Urolova Laylo Zuxriddin qizi",
-  "Xakimova O’giloy Furqat qizi",
+  "Xakimova O‘giloy Furqat qizi",
   "Xamidov Shohjahon Shuxrat o'g'li",
-  "Yo`Ldoshboyeva Rahima Hajixon Qizi",
+  "Yo‘ldoshboyeva Rahima Hajixon qizi",
   "Yormatova Dilnura Latif qizi",
-  "Zayniddinova Mohichehra Xamroz Qizi",
+  "Zayniddinova Mohichehra Xamroz qizi",
   "Shoqulova Sevinch Orzibek qizi",
   "Shovdirova Marjona Raximjon qizi",
-  "Худайберганова Умида Равшан кизи",
+  "Xudayberganova Umida Ravshan qizi"
 ];
 
 const studentList = document.getElementById("studentList");
@@ -58,26 +58,26 @@ const schedule = {
     "08:45 - Ona tili",
     "09:35 - Rus tili",
     "10:20 - Ingliz tili",
-    "11:05 - Matematika"
+    "11:05 - Matematika",
     "11:55 - Adabiyot"
   ],
   "Payshanba": [
     "08:00 - Ona tili",
     "08:45 - Musiqa",
-    "09:30 - Inglis tili",
+    "09:30 - Ingliz tili",
     "10:15 - Tarix",
     "11:00 - Tarbiya"
   ],
   "Juma": [
-    "08:00,08:45  - Texnologiya",
-    "09:30 - Matematika",
-    "10:15 - Tabiiy fan",
-    "11:00 - Informatika",
-    "11:45 - Jismoniy tarbiya"
+    "08:00 - Texnologiya",
+    "08:45 - Matematika",
+    "09:30 - Tabiiy fan",
+    "10:15 - Informatika",
+    "11:00 - Jismoniy tarbiya"
   ],
   "Shanba": [
     "08:00 - Ingliz tili",
-    "08:45 - Rus tili"
+    "08:45 - Rus tili",
     "09:30 - Matematika",
     "10:15 - Ona tili",
     "11:00 - Adabiyot"
@@ -122,4 +122,3 @@ const themeBtn = document.getElementById("themeBtn");
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
-
