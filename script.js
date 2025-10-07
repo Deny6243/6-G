9cdf040c1ba2b9e0206b69bd66cbfd0894b26794
Deny6243 <saidodilkhon12@gmail.com>
2025-10-07 19:25:13 +0500

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 // === O'QUVCHILAR ===
 const students = [
-  "Abdullayeva Sofiya",
-  "Kadirova Malohat",
-  "Rustamov Bekzod",
-  "Tursunboyev Muhammad",
-  "Saidov Zafar",
-  "Yusupova Madina",
-  "To‘xtasinova Dilrabo",
-  "Karimov Davron",
-  "Islomova Shahlo",
-  "Sharipov Aziz",
-  "Mamatov Javohir",
-  "Rasulova Shahrizoda",
-  "Ergashev Ibrohim",
-  "Murodov Otabek",
-  "Tursunova Nilufar",
-  "Saidov Kamol",
-  "Ravshanov Nodir",
-  "Xudoyberdiyeva Mohira"
+  "Al Orifxo'ja Said Odilxon Said Olimxon o'g'li",
+  "Ashurov Humoyun Zamir O'g'li",
+  "	Botiraliyeva Aziza Erali qizi",
+  "Davranov Muhammadiso Jamol o’g’li",
+  "Fayzullayev Elshodbek Elbek o’g’li",
+  "Husanova Mushtariy Ergash qizi",
+  "Ilxamboyev Xurrambek Gulomjon O'g'li",
+  "Ismoilov Muslimbek Isomiddinovich",
+  "Jurayeva Dilnura Rustamovna",
+  "Matyoqubova Madinabonu G`ayrat qizi",
+  "Muxtarov Sunnatxon Batir o’g’li",
+  "Nasrullayeva Hadicha Talibjonovna",
+  "Ochilov Mo'minjon Ma'ruf O'g'li",
+  "Pardaboyeva Dariya Yermanovna",
+  "Rajabaliyeva Shirinabonu Sobir qizi",
+  "Sobirova Robiya Shavkat qizi",
+  "Tirkashova Ruhshona Rustam qizi",
+  "Turdibayev Muhammadali Nosirjon o'g'li",
+  "Tuxtasinova Madinabonu Murodjon qizi",
+  "Urazbekova Diana Toxir qizi",
+  "Urolova Laylo Zuxriddin qizi",
+  "Xakimova O’giloy Furqat qizi",
+  "Xamidov Shohjahon Shuxrat o'g'li",
+  "Yo`Ldoshboyeva Rahima Hajixon Qizi",
+  "Yormatova Dilnura Latif qizi",
+  "Zayniddinova Mohichehra Xamroz Qizi",
+  "Shoqulova Sevinch Orzibek qizi",
+  "Shovdirova Marjona Raximjon qizi",
+  "Худайберганова Умида Равшан кизи",
 ];
 
 const studentList = document.getElementById("studentList");
@@ -40,34 +51,36 @@ const schedule = {
     "08:00 - Tarix",
     "08:45 - Matematika",
     "09:30 - Tasviriy san’at",
-    "10:15 - Jismoniy tarbiya",
-    "11:00 - Ingliz tili"
+    "10:15 - Jismoniy tarbiya"
   ],
   "Chorshanba": [
     "08:00 - Tabiiy fan",
     "08:45 - Ona tili",
-    "09:30 - Rus tili",
-    "10:15 - Ingliz tili",
-    "11:00 - Adabiyot"
+    "09:35 - Rus tili",
+    "10:20 - Ingliz tili",
+    "11:05 - Matematika"
+    "11:55 - Adabiyot"
   ],
   "Payshanba": [
-    "08:00 - Matematika",
-    "08:45 - Texnologiya",
-    "09:30 - Ona tili",
-    "10:15 - Informatika",
+    "08:00 - Ona tili",
+    "08:45 - Musiqa",
+    "09:30 - Inglis tili",
+    "10:15 - Tarix",
     "11:00 - Tarbiya"
   ],
   "Juma": [
-    "08:00 - Texnologiya",
-    "08:45 - Rus tili",
+    "08:00,08:45  - Texnologiya",
     "09:30 - Matematika",
-    "10:15 - Ona tili",
-    "11:00 - Jismoniy tarbiya"
+    "10:15 - Tabiiy fan",
+    "11:00 - Informatika",
+    "11:45 - Jismoniy tarbiya"
   ],
   "Shanba": [
     "08:00 - Ingliz tili",
-    "08:45 - Matematika",
-    "09:30 - Adabiyot"
+    "08:45 - Rus tili"
+    "09:30 - Matematika",
+    "10:15 - Ona tili",
+    "11:00 - Adabiyot"
   ]
 };
 
@@ -109,3 +122,4 @@ const themeBtn = document.getElementById("themeBtn");
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
